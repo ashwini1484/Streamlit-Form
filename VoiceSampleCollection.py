96268d986fb04c44ede1127334664f6571777c9c
Ashwini M <ashwini1484@users.noreply.github.com>
2023-12-11 14:55:43 +0530

    Changing appearance of recorder

--- VoiceSampleCollection.py
+++ VoiceSampleCollection.py
@@ -1,16 +1,18 @@
 import streamlit as st
-from audiorecorder import audiorecorder
+#from audiorecorder import audiorecorder
+from audio_recorder_streamlit import audio_recorder
 
 with st.form("my_form"):
 
    st.title("Audio Recorder")
-   audio = audiorecorder("Click to record", "Click to stop recording")
+   audio_bytes = audio_recorder("Click to record", "Click to stop recording")
 
-   print(len(audio))
+   if audio_bytes:
+       st.audio(audio_bytes, format="audio/wav")
 
    submit_clicked = st.form_submit_button('Submit')
 
    if submit_clicked:
-      print(len(audio))
+       print(len(audio_bytes))
 
 st.write("Outside the form")
