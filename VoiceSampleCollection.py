ebda5efdcef0b5d483e7b425e9135684922bb415
Ashwini M <ashwini1484@users.noreply.github.com>
2023-12-11 14:59:41 +0530

    Update code for right package

--- VoiceSampleCollection.py
+++ VoiceSampleCollection.py
@@ -1,11 +1,11 @@
 import streamlit as st
 #from audiorecorder import audiorecorder
-from audio_recorder_streamlit import audio_recorder
+import audio_recorder_streamlit as ars
 
 with st.form("my_form"):
 
    st.title("Audio Recorder")
-   audio_bytes = audio_recorder("Click to record", "Click to stop recording")
+   audio_bytes = ars.audio_recorder("Click to record", "Click to stop recording")
 
    if audio_bytes:
        st.audio(audio_bytes, format="audio/wav")
