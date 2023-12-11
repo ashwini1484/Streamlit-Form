0c87de9e480e183af8e078b42a3868f2ae92645b
Ashwini M <ashwini1484@users.noreply.github.com>
2023-12-11 15:11:07 +0530

    update sample rate

--- VoiceSampleCollection.py
+++ VoiceSampleCollection.py
@@ -5,10 +5,10 @@ import audio_recorder_streamlit as ars
 with st.form("my_form"):
 
    st.title("Audio Recorder")
-   audio_bytes = ars.audio_recorder("Apple", "Click to stop recording")
+   audio_bytes = ars.audio_recorder("Apple",pause_threshold=3.0)
 
    if audio_bytes:
-       st.audio(audio_bytes, format="audio/wav")
+       st.audio(audio_bytes, format="audio/wav",sample_rate=44100)
 
    # submit_clicked = st.form_submit_button('Submit')
 
